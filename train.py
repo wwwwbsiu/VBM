be045d7c661c5e1c0674c1392572b3aec4e718c6
wwwwbsiu <bw23288@gmail.com>
2024-11-07 15:54:23 +0800

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torch
 
 
 if __name__ == '__main__':
-    model = YOLO(r"ultralytics\cfg\models\v8\VBM_YOLO_AGB.yaml")  # load model cfg
+    model = YOLO(r"ultralytics\cfg\models\VBM\VBM_YOLO_AGB.yaml")  # load model cfg
     # model.load(r'') # pre-trained weights
     model.train(
         data=r'ultralytics\cfg\car.yaml', # load dataset cfg
